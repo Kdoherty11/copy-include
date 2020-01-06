0ba64eb930c5d15a8ed0c8a7a04e388422c84f8e
Kevin Doherty <kdoherty@fb.com>
2020-01-06 15:52:49 -0500

    Initial impl

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,68 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function getHeaderFilePath(path: string): string | null {
+	if (path.endsWith('-inl.h')) {
+		return path.substring(0, path.length - 6) + '.h';
+	}
+	if (path.endsWith(".h")) {
+		return path;
+	}
+	if (path.endsWith('.cpp')) {
+		return path.substring(0, path.length - 4) + '.h';
+	}
+	return null;
+}
+
+function getRelativePath(path: string): string {
+	let relativePath = vscode.workspace.asRelativePath(path);
+	return relativePath.replace(/\\/g, '/');
+}
+
+function shouldUseAngleBrackets(headerPath: string): boolean {
+	const prefixes = ["folly"];
+	return prefixes.some(prefix => headerPath.startsWith(prefix));
+}
+
+function getIncludeString(headerPath: string): string {
+	return shouldUseAngleBrackets(headerPath) ?
+		`#include <${headerPath}>` :
+		`#include "${headerPath}"`;
+}
+
+// file:///Documents/foo.h => /Documents/foo.h
+function removeFilePrefix(path: string): string {
+	const filePrefix = 'file://';
+	if (!path.startsWith(filePrefix)) {
+		throw new Error(`Expected ${path} to start with ${filePrefix}`);
+	}
+	return path.substring(filePrefix.length);
+}
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copyinclude" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	const disposable = vscode.commands.registerCommand('extension.copyInclude', async (uri: string) => {
+		if (!uri && vscode.window.activeTextEditor) {
+			uri = vscode.window.activeTextEditor.document.uri.toString();
+		}
+		if (!uri) {
+			vscode.window.showErrorMessage('Cannot copy include, no file is opened');
+			return;
+		}
+		const headerPath = getHeaderFilePath(uri);
+		if (!headerPath) {
+			vscode.window.showErrorMessage('Unable to determine header path: ' + uri);
+			return;
+		}
+		if (!fs.existsSync(removeFilePrefix(headerPath))) {
+			vscode.window.showErrorMessage('Header not found: ' + headerPath);
+			return;
+		}
+		const relativeHeaderPath = getRelativePath(headerPath);
+		const include = getIncludeString(removeFilePrefix(relativeHeaderPath));
+		await vscode.env.clipboard.writeText(include);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
